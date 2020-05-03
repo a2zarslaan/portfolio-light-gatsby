7128036aa451badc6d3619ff7b9682573aae1a7e
Amar Arslaan <a2zarslaan@hotmail.com>
2020-05-03 20:46:34 +0100

    stylised projects list on the project page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,7 +4,6 @@ import { Link, graphql, useStaticQuery } from "gatsby";
 
 import Layout from "components/Layout";
 import Container from "components/Container";
-import ProjectComp from "components/ProjectComp";
 
 const ProjectsPage = () => {
   const data = useStaticQuery(graphql`
@@ -33,20 +32,14 @@ const ProjectsPage = () => {
         <title>Projects</title>
       </Helmet>
       <Container>
-        <h1 className="project-title">Projects</h1>
-        {/* <ProjectComp
-          title={data.allMarkdownRemark.edges.node.frontmatter.title}
-          date={data.allMarkdownRemark.edges.node.frontmatter.date}
-          html={data.allMarkdownRemark.edges.node.html}
-        /> */}
-
-        <ol className="list">
+        <h1>Projects</h1>
+        <ol>
           {data.allMarkdownRemark.edges.map((edge) => {
             return (
-              <li className="list__element">
+              <li>
                 <Link to={`/projects/${edge.node.fields.slug}`}>
                   <h2>{edge.node.frontmatter.title}</h2>
-                  <p>{edge.node.frontmatter.date}</p>
+                  {/* <p>{edge.node.frontmatter.date}</p> */}
                 </Link>
               </li>
             );
